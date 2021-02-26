a8e3a029f22dbe429e0285ef06a9297015b39cc1
Fabiana Viana <fabiana.viana@reclameaqui.com.br>
2021-02-25 23:25:14 -0300

    start nlw project

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.experienceBar}>
+      <ExperienceBar />
+      <section>
+        <div className={styles.profileContainer}  >
+          <Profile />
+        </div>
+        <div>
+        </div>
+      </section>
+    </div>
+  )
+}
